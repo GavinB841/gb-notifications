bb64aa584614bc9f585f8dc90f3eb3498e979ea9
Gavin Boyle <gboyle@nightline-delivers.com>
2019-11-18 10:03:46 +0000

    feature(gb-notifications): fix tests

--- projects/gb-notifications-showcase/src/app/app.component.spec.ts
+++ projects/gb-notifications-showcase/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { GbNotificationsModule } from '../../../gb-notifications/src/lib/gb-notifications.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [GbNotificationsModule]
     }).compileComponents();
   }));
 
@@ -12,18 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'gb-notifications-showcase'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gb-notifications-showcase');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to gb-notifications-showcase!');
   });
 });
